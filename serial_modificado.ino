23c528e4f2673386d1ecca16585aaf00c46de108
Willian B. Ribeiro <64652475+Willian-R@users.noreply.github.com>
2021-10-01 18:59:38 -0300

    serial

---
+++ serial_modificado.ino
@@ -0,0 +1,10 @@
+// Código 3 - Serial
+
+void setup() // roda apenas uma vez, ao iniciar o programa
+   {
+   Serial.begin(9600); // configura a serial para 9600
+   }
+void loop() // fica rodando continuamente
+   {
+   Serial.println("Olá! Sou o Arduino!"); // envia a mensagem pela serial
+   }
